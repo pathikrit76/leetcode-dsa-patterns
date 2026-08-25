121705931ecb64c3ecc0909a6bb0ac3ef45e578f
pathikrit76 <84118788+pathikrit76@users.noreply.github.com>
2026-08-26 03:08:19 +0530

    Add LC 209 minimum size subarray solution

---
+++ 01-sliding-window/variable/LC209MinimumSizeSubarraySum.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int minSubArrayLen(int target, int[] nums) {
+        int left = 0;
+        int windowSum = 0;
+        int minLength = Integer.MAX_VALUE;
+
+        for (int right = 0; right < nums.length; right++) {
+            windowSum += nums[right];
+
+            while (windowSum >= target) {
+                minLength = Math.min(minLength, right - left + 1);
+                windowSum -= nums[left];
+                left++;
+            }
+        }
+
+        return minLength == Integer.MAX_VALUE ? 0 : minLength;
+    }
+}
