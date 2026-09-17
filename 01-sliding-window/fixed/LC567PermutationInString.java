be53fddb76341ddf7cbff0ca61d97cb3a7c4f720
pathikrit76 <84118788+pathikrit76@users.noreply.github.com>
2026-09-18 03:47:53 +0530

    Add LC567 Permutation in String

---
+++ 01-sliding-window/fixed/LC567PermutationInString.java
@@ -0,0 +1,39 @@
+class Solution {
+    public boolean checkInclusion(String s1, String s2) {
+        if (s1.length() > s2.length()) {
+            return false;
+        }
+
+        int[] freq1 = new int[26];
+        int[] freq2 = new int[26];
+
+        for (int i = 0; i < s1.length(); i++) {
+            freq1[s1.charAt(i) - 'a']++;
+            freq2[s2.charAt(i) - 'a']++;
+        }
+
+        if (isSame(freq1, freq2)) {
+            return true;
+        }
+
+        for (int i = s1.length(); i < s2.length(); i++) {
+            freq2[s2.charAt(i) - 'a']++;
+            freq2[s2.charAt(i - s1.length()) - 'a']--;
+
+            if (isSame(freq1, freq2)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private boolean isSame(int[] freq1, int[] freq2) {
+        for (int i = 0; i < 26; i++) {
+            if (freq1[i] != freq2[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
